f1ab5ce182f1b74883ae7a187e716b1a067722f8
DJWL <58622651+djwlx@users.noreply.github.com>
2026-09-04 01:41:02 +0000

    feat: use react

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,8 @@
-import { render } from 'preact';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-render(<App />, document.getElementById('root')!);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import preact from '@preact/preset-vite';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': `${import.meta.dirname}/src`,
