1226ffdbe72dacbd3b3be968937a29dcbed02843
DJWL <58622651+djwlx@users.noreply.github.com>
2026-09-09 06:47:04 +0000

    feat: use base ui

--- src/apps/home/index.tsx
+++ src/apps/home/index.tsx
@@ -1,8 +1,14 @@
 import { useUser } from '@/stores/user';
+import { Button } from '@/components/ui/button';
 
 export function Home() {
   const user = useUser((state) => state.user);
   const increase = useUser((state) => state.increasePopulation);
 
-  return <div onClick={increase}>{user}</div>;
+  return (
+    <div>
+      {user}
+      <Button onClick={increase}>Click me</Button>
+    </div>
+  );
 }
