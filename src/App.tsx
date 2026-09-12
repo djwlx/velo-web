f1e93a5a8bb0e51da296554bbbb970f15917af49
DJWL <1602951127@qq.com>
2026-09-12 19:25:55 +0800

    feat: remove tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Route, Switch } from 'wouter';
 import { Home } from './apps/home';
-import { Tools } from './apps/tools';
 import { FileList115 } from './apps/file-list-115';
 import { Pic } from './apps/pic';
 import { NotFound } from './apps/not-found';
@@ -9,7 +8,6 @@ function App() {
   return (
     <Switch>
       <Route path="/" component={Home} />
-      <Route path="/tools" component={Tools} />
       <Route path="/115" component={FileList115} />
       <Route path="/pic" component={Pic} />
       <Route component={NotFound} />
--- src/apps/tools/index.tsx
+++
@@ -1,3 +0,0 @@
-export function Tools() {
-  return <div>tools</div>;
-}
